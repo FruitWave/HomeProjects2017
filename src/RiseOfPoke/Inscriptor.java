7c984e7cf2751f12acc4d46098968a84f626846a
FruitWave <fritzcholar@Wendys-iMac.local>
2017-06-13 10:47:42 -0700

    Commit 2-ROP
    Small commit ( appointment, gtg!)
    cya!

--- src/RiseOfPoke/Inscriptor.java
+++ src/RiseOfPoke/Inscriptor.java
@@ -1,5 +1,7 @@
 package RiseOfPoke;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -20,6 +22,25 @@ public class Inscriptor extends JPanel implements ActionListener, KeyListener {
 		gameSpeed = new Timer(1000 / 120, this);
 	}
 
+	
+
+	public void paintComponent(Graphics danna) {
+		// if (currentState == MENU_STATE) {
+		// drawMenuState(delta);
+		// } else if (currentState == GAME_STATE) {
+		// drawGameState(delta);
+		// } else if (currentState == END_STATE) {
+		// drawEndState(delta);
+		// }
+		drawGameState(danna);
+	}
+
+	void drawGameState(Graphics b) {
+		b.setColor(Color.darkGray);
+		b.fillRect(0, 0, 1000, 1000);
+		book.draw(b);
+		//show score code
+	}
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
--- src/RiseOfPoke/Poker.java
+++ src/RiseOfPoke/Poker.java
@@ -1,12 +1,30 @@
 package RiseOfPoke;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+
 public class Poker extends GameObject {
 
-	public int health;
+	int health;
+	Color color = new Color(health, health, health);
 
 	public Poker(int x, int y, int width, int height, int health) {
 		super(x, y, width, height);
 		this.health = health;
 	}
 
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		super.update();
+
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(color);
+		g.drawOval(x, y, width, height);
+	}
 }
